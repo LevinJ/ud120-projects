f268f30086ad1c4de56ed6a98aaa31ef64baba97
Levin Jian <jianzhirong@gmail.com>
2016-01-05 20:03:02 +0800

    svm linear model

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -26,5 +28,14 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 #########################################################
+clf = SVC(kernel='linear')
+t0 = time()
+clf = clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t0 = time()
+y_pred=clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
 
+accuracy = accuracy_score(labels_test,y_pred )
+print "accuracy is %f" %(accuracy)
 
