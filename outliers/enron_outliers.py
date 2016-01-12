5ff21e3fbceeee83784cba8fafa3e67bd1ee3600
Levin Jian <jianzhirong@gmail.com>
2016-01-12 15:53:00 +0800

    enron outlier detection

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -9,11 +9,29 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict.pop('TOTAL', 0)
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
 
 ### your code below
+def isNaN(num):
+    return num == "NaN"
+def findLargestOutlier():
+    for key, value in data_dict.iteritems():
+        if not (isNaN(value['salary']) and isNaN(value['bonus'])):
+            if (value['salary']> 1e6 and value['bonus']> 5e6):
+                print "maxkey", key, value['salary'], value['bonus']
+    return
 
+findLargestOutlier()
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
+
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
 
 
