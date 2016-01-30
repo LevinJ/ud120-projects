20dab0ebd35a4475da9c76eae55bc8d70ecad091
Levin Jian <jianzhirong@gmail.com>
2016-01-30 11:11:12 +0800

    display basic statistics

--- final_project/data_exploration.py
+++ final_project/data_exploration.py
@@ -12,40 +12,54 @@ import pandas as pd
 
 
 class Data_Exploration_Proxy:
-    def testInit(self):
-        with open("final_project_dataset.pkl", "r") as data_file:
-            data_dict = pickle.load(data_file)
-        features_list = data_dict['ALLEN PHILLIP K'].keys()
+    def __init__(self,data_dict):
+        self.data_dict = data_dict
+        return
+    def run(self):
+        self.__dispBasicStatistics(self.__getAllFeatures())
+        return
+    def __identifyAllZeros(self, features_list):
+        #identify the record which has all zeros features
+        if features_list[0] == 'poi':
+            features_list = features_list[1:]
+        else:
+            features_list = features_list
+            
+        for key in sorted(self.data_dict.keys()):
+            isAllzeros = True
+            for feature in features_list:
+                if self.data_dict[key][feature] !=  'NaN':
+                    isAllzeros = False
+            if isAllzeros:
+                print 'record ', key, ' is all zeroes and has been removed from samples'
+        return
+    def __getAllFeatures(self):
+        features_list = self.data_dict['ALLEN PHILLIP K'].keys()
         features_list.remove('poi')
         features_list.insert(0, 'poi')
-        my_dataset = data_dict
-        data = featureFormat(my_dataset, features_list, remove_NaN=False, remove_all_zeroes=False, remove_any_zeroes=False, sort_keys = True)
+        features_list.remove('email_address')
+        return features_list
+    def __dispBasicStatistics(self, features_list):
+        print "display basic statistics about the data set"
+        data = featureFormat(self.data_dict, features_list, sort_keys = True)
         labels, features = targetFeatureSplit(data)
-        return labels, features,features_list
-    def getBasicStatistics(self):
-        labels,features,features_list = self.testInit()
-        self.savetoCSV(labels,features,features_list)
-        features_df = pd.DataFrame(features, columns=features_list[1:])
-        print features_df.describe()
+        print "features used: ", features_list
+        print "number of total existing features", features[0].shape[0]
         print "total number of data points: ", len(labels)
-        print "allocation across classes (POI/non-POI)", np.array(labels).sum()/len(labels)
-        print "number of total existing features", len(features_list)
-        return
-    def savetoCSV(self,labels,features,features_list):
-        labels = np.array(labels).reshape((len(labels),1))
-        data = np.hstack((labels, np.array(features)))
-        df = pd.DataFrame(data, columns=features_list)
-        df.to_csv('enron_data.csv')
-        return
-    def removeOutlier(self):
+        print "total number of Poi", (np.array(labels) == 1).sum()
+        print "total number of non_Poi", len(labels) - (np.array(labels) == 1).sum()
+        print "allocation across classes (POI/non-POI)", (np.array(labels) == 1).sum()/float(len(labels))
+        self.__identifyAllZeros(features_list)
         return
     
 
 # print features
 
 def main():
-    test =  Data_Exploration_Proxy() 
-    test.getBasicStatistics()
+    with open("final_project_dataset.pkl", "r") as data_file:
+        data_dict = pickle.load(data_file)
+    test =  Data_Exploration_Proxy(data_dict) 
+    test.run()
 
 if __name__ == '__main__':
     main()
