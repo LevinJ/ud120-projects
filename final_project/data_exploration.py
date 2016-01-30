b36fb2982533aa169ba4f7c2b1c8637e8c6f6067
Levin Jian <jianzhirong@gmail.com>
2016-01-30 11:33:05 +0800

    save selected features data to csv

--- final_project/data_exploration.py
+++ final_project/data_exploration.py
@@ -16,10 +16,14 @@ class Data_Exploration_Proxy:
         self.data_dict = data_dict
         return
     def run(self):
-        self.__dispBasicStatistics(self.__getAllFeatures())
+#         features_list = self.__getAllFeatures()
+        features_list = ['poi','salary','bonus']
+        self.__dispBasicStatistics(features_list)
+        self.__saveToCsv(features_list)
         return
     def __identifyAllZeros(self, features_list):
         #identify the record which has all zeros features
+        numAllZeros = 0
         if features_list[0] == 'poi':
             features_list = features_list[1:]
         else:
@@ -32,6 +36,8 @@ class Data_Exploration_Proxy:
                     isAllzeros = False
             if isAllzeros:
                 print 'record ', key, ' is all zeroes and has been removed from samples'
+                numAllZeros = numAllZeros + 1
+        print "Total number of removed all zeros records; ", numAllZeros
         return
     def __getAllFeatures(self):
         features_list = self.data_dict['ALLEN PHILLIP K'].keys()
@@ -39,17 +45,23 @@ class Data_Exploration_Proxy:
         features_list.insert(0, 'poi')
         features_list.remove('email_address')
         return features_list
+    def __saveToCsv(self,features_list):
+        print "Save selected data to csv"
+        data = featureFormat(self.data_dict, features_list, sort_keys = True)
+        df = pd.DataFrame(data, columns=features_list)
+        df.to_csv('selecteddata.csv')
+        return
     def __dispBasicStatistics(self, features_list):
         print "display basic statistics about the data set"
         data = featureFormat(self.data_dict, features_list, sort_keys = True)
         labels, features = targetFeatureSplit(data)
+        self.__identifyAllZeros(features_list)
         print "features used: ", features_list
         print "number of total existing features", features[0].shape[0]
         print "total number of data points: ", len(labels)
         print "total number of Poi", (np.array(labels) == 1).sum()
         print "total number of non_Poi", len(labels) - (np.array(labels) == 1).sum()
         print "allocation across classes (POI/non-POI)", (np.array(labels) == 1).sum()/float(len(labels))
-        self.__identifyAllZeros(features_list)
         return
     
 
